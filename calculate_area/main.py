3dd59197173f4e9fa7ef4ca6df66254faffada07
Maia Radulescu <maiateorad@gmail.com>
2022-03-13 14:17:39 +0200

    fix: typo

--- calculate_area/main.py
+++ calculate_area/main.py
@@ -13,4 +13,4 @@ shape_p = (shape_l + shape_w) * 2  # calculate perimeter
 if shape_w == shape_l:  # check is the shape is a square
     print(f'This is a square and area is {shape_a} and perimeter is {shape_p}')
 elif shape_w != shape_l:  # check if the shape is a rectangle
-    print(f'This is a rectangle and are is {shape_a} and perimeter is {shape_p}')
+    print(f'This is a rectangle and area is {shape_a} and perimeter is {shape_p}')
