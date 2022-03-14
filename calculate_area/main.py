0d9815cc459aa048f85a5ac0e144316deb54c32e
Maia Radulescu <maiateorad@gmail.com>
2022-03-14 12:35:51 +0200

    improvement: create function

--- calculate_area/main.py
+++ calculate_area/main.py
@@ -6,11 +6,24 @@
 shape_w = int(input("Enter  width \n"))  # shape  width
 shape_l = int(input("Enter length \n"))  # shape  length
 
+
+#  Function
+
+def cal_perimeter(a, b):
+    result = (a + b) * 2
+    return result
+
+
+def cal_area(a, b):
+    result = a * b
+    return result
+
+
 shape_a = shape_w * shape_l  # calculate area
 
 shape_p = (shape_l + shape_w) * 2  # calculate perimeter
 
 if shape_w == shape_l:  # check is the shape is a square
-    print(f'This is a square and area is {shape_a} and perimeter is {shape_p}')
+    print(f'This is a square and area is {cal_area(shape_w, shape_l)} and perimeter is {cal_perimeter(shape_w, shape_l)}')
 elif shape_w != shape_l:  # check if the shape is a rectangle
-    print(f'This is a rectangle and area is {shape_a} and perimeter is {shape_p}')
+    print(f'This is a rectangle and area is {cal_area(shape_l, shape_w)} and perimeter is {cal_perimeter(shape_w, shape_l)}')
